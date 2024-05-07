e880c12313dee617796e143a527c24be315b6e50
Rumindu De Silva <rumindu.desilva@itelasoft.com>
2024-05-07 17:00:41 +0530

    styling the auth0 loader

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/styles.css";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import { Auth0ProviderWithNavigate } from "./Auth0ProviderWithNavigate";
