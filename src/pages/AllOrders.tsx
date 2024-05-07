f933784518877647dc31fba5b89fc521b8e826b5
Rumindu De Silva <rumindu.desilva@itelasoft.com>
2024-05-07 14:30:39 +0530

    View all order history page partially complete

--- src/pages/AllOrders.tsx
+++ src/pages/AllOrders.tsx
@@ -4,94 +4,42 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { setAuthToken } from "../config/axiosConfig";
 import { getAllOrdersService } from "../services/getAllOrdersService";
 
+type Product = {
+  productId: number;
+  name: string;
+  quantity: number;
+  price: string;
+  image: string;
+};
+
+type Order = {
+  id: number;
+  sub: string;
+  createdAt: string;
+  first_name: string;
+  last_name: string;
+  address_line1: string;
+  address_line2: string;
+  zip_code: string;
+  shipping_method: string;
+  orderInfo: Product[];
+};
+
+type Orders = Order[];
+
 export const AllOrders: React.FC = () => {
-//   const { getAccessTokenSilently } = useAuth0();
-//   const [orderList, setOrderList] = useState<any>([]);
+  const { getAccessTokenSilently } = useAuth0();
+  const [orders, setOrders] = useState<Orders>([]);
 
-//   const getAllOrderHistory= async () => {
-//     const accessToken = await getAccessTokenSilently();
-//     await setAuthToken(accessToken);
-//     const response = await getAllOrdersService(accessToken);
-//     setOrderList(response.data.data);
-  
-//   }
-
-
-//   useEffect(() => {
-//     getAllOrderHistory();
-//   }, []);
-// console.log(orderList);
-  const response = {
-    "message": "success",
-  "data": {
-    "message": "success",
-    "data": [
-      {
-        "id": 67,
-        "sub": "auth0|65f9a317a9f6e269742059db",
-        "createdAt": "2024-05-06T16:22:06.000Z",
-        "first_name": "Rumindu",
-        "last_name": "De Silva",
-        "address_line1": "No 25, School lane,",
-        "address_line2": "Colombo 03",
-        "zip_code": "8080",
-        "shipping_method": "Standard",
-        "orderInfo": [
-          {
-            "productId": 2,
-            "name": "Computer",
-            "quantity": 1,
-            "price": "30000.00",
-            "image": "/imgs/computer.jpg"
-          },
-          {
-            "productId": 3,
-            "name": "Banana",
-            "quantity": 1,
-            "price": "200.00",
-            "image": "/imgs/banana.jpg"
-          }
-        ]
-      },
-      {
-        "id": 68,
-        "sub": "auth0|65f9a317a9f6e269742059db",
-        "createdAt": "2024-05-06T16:53:43.000Z",
-        "first_name": "Kavishka",
-        "last_name": "Wijeratrhna",
-        "address_line1": "No23,",
-        "address_line2": "Main street, Ambalangoda",
-        "zip_code": "8083",
-        "shipping_method": "Standard",
-        "orderInfo": [
-          {
-            "productId": 1,
-            "name": "Book",
-            "quantity": 3,
-            "price": "100.00",
-            "image": "/imgs/book.jpg"
-          },
-          {
-            "productId": 2,
-            "name": "Computer",
-            "quantity": 1,
-            "price": "30000.00",
-            "image": "/imgs/computer.jpg"
-          },
-          {
-            "productId": 3,
-            "name": "Banana",
-            "quantity": 1,
-            "price": "200.00",
-            "image": "/imgs/banana.jpg"
-          }
-        ]
-      }
-    ]
-  }
-}
-
-  const orders = response.data.data;
+  const getAllOrderHistory = async () => {
+    const accessToken = await getAccessTokenSilently();
+    await setAuthToken(accessToken);
+    const response = await getAllOrdersService(accessToken);
+    setOrders(response.data);
+  };
+  useEffect(() => {
+    getAllOrderHistory();
+  }, []);
 
   return (
     <Container>
